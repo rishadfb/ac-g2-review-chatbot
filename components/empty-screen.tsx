1aa480540566364cee82b8e30fbfe4dff3494ddb
Rishad Bharucha <rb2102@gmail.com>
2024-04-27 16:50:22 -0500

    update empty screen welcome message

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,7 +1,6 @@
 import { UseChatHelpers } from 'ai/react'
 
 import { Button } from '@/components/ui/button'
-import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
@@ -24,12 +23,10 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to the Supabaseified Next.js AI Chatbot!
+          Welcome to the ActiveCampaign G2 Reviews AI Chatbot!
         </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://supabase.com">Supabase</ExternalLink>.
+        <p className="mb-6 leading-normal text-muted-foreground">
+          Gain insights into our customers and improve our product quality.
         </p>
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
